5bcefc34b9d8569e791a7205d440f1455f7bf7e0
TahjaeDev <88630190+TahjaeDev@users.noreply.github.com>
2023-08-06 14:10:35 +0000

    updated index and stuff

---
+++ script.js
@@ -0,0 +1,22 @@
+var chatbox = document.getElementById("TosAI");
+var userinput = document.getElementById("userinput");
+
+function send() {
+    var message = userinput.value;
+    userinput.value = "";
+    var response = "I'm sorry, I don't understand.";
+    if (message == "Hello") {
+        response = "Hi there!";
+    } else if (message == "How are you?") {
+        response = "I'm doing well, thank you.";
+    } else if (message == "What's your name?") {
+        response = "My name is TosAI.";
+    }
+    var messageElement = document.createElement("p");
+    messageElement.innerHTML = "<strong>You:</strong> " + message;
+    var responseElement = document.createElement("p");
+    responseElement.innerHTML = "<strong>Chatbot:</strong> " + response;
+    chatbox.appendChild(messageElement);
+    chatbox.appendChild(responseElement);
+    chatbox.scrollTop = chatbox.scrollHeight;
+}
