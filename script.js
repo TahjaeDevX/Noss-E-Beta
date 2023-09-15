197bda2223a67816ece2a68e83e75689c7df7cac
TahjaeDev <88630190+TahjaeDev@users.noreply.github.com>
2023-09-15 14:02:53 -0400

    Update script.js

--- script.js
+++ script.js
@@ -10,10 +10,10 @@ function send() {
     } else if (message == "How are you?") {
         response = "I'm doing well, thank you.";
     } else if (message == "What's your name?") {
-        response = "My name is TosAI. Made By SkyTos Studios";
+        response = "My name is TosAI. Made By TahjaeDev";
     }
     else if (message == "Who are you?")
-        response = "I am TosAI, an"
+        response = "I am TosAI, an AI interface created by TahjaeDev, I can write stuff for you, but I am stillin beta"
     
     var messageElement = document.createElement("p");
     messageElement.innerHTML = "<strong>You:</strong> " + message;
