07d43451b22ec784da265b0207da3a2a44f39831
TahjaeDevALT <129321089+TahjaeDevX@users.noreply.github.com>
2023-11-13 08:47:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,9 @@ function send() {
     } else if (message == "What's your name?") {
         response = "My name is TosAI. Made By TahjaeDev";
     } else if (message == "Who are you?")
-        response = "I am TosAI, an AI interface created by TahjaeDev, I can write stuff for you, but I am stillin beta"
+        response = "I am TosAI, an AI interface created by TahjaeDev, I can write stuff for you, but I am still in beta";
+    } else if (message == "Make a Video")
+        response = "Sorry I cant make a video yet.";
     
     var messageElement = document.createElement("p");
     messageElement.innerHTML = "<strong>You:</strong> " + message;
